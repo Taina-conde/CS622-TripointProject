04bb472a8a9727f798a62549ee3a7e00bc13e881
Taina Conde <tainabatistaconde@gmail.com>
2022-01-17 16:24:49 -0500

    add attributes to CreditCard abstract class and use CreditCard in Transaction

--- CreditCard.java
+++ CreditCard.java
@@ -1,2 +1,10 @@
 public class CreditCard {
+    protected int diningValue;
+    protected int groceryValue;
+    protected int onlineShopValue;
+    protected int travelValue;
+    protected String type;
+
+
+
 }
--- Transaction.java
+++ Transaction.java
@@ -1,18 +1,18 @@
 public abstract class Transaction {
     protected double amount;
-    protected cardType;
+    protected CreditCard card;
     protected String category;
 
     public Transaction() {
 
     }
-    public Transaction(String cardType, String category, double amount ) {
+    public Transaction(CreditCard card, String category, double amount ) {
         this.amount = amount;
-        this.cardType = cardType;
+        this.card = card;
         this.category = category;
     }
     public abstract double getAmount();
-    public abstract String getCardType();
+    public abstract CreditCard getCard();
     public abstract String getCategory();
 
 }
