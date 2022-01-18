b2d25ff629539d34d6297f55a5a77cbb1dab69f3
Taina Conde <tainabatistaconde@gmail.com>
2022-01-17 21:14:15 -0500

    Remove other categories transaction classes

--- TransactionTest.java
+++ TransactionTest.java
@@ -2,16 +2,17 @@
 class TransactionTest {
     public static void main(String[] args) {
         CreditCard basicCard = new BasicCard("Taina", 5000);
-        CreditCard preferredCard = new BasicCard("Taina");
+        CreditCard preferredCard = new PreferredCard("Taina");
         Transaction newTrans = new CategoryTransaction(basicCard,"travel", 55.60);
         //testing method calculatePoints
         System.out.println(basicCard.getPointsBal());
         int pointsEarned = newTrans.calculatePoints();
         System.out.println(pointsEarned);
         assert pointsEarned == 278 : "The method calculatePoints returned " + pointsEarned + " but it should have returned 278";
-        newTrans = new CategoryTransaction(preferredCard,"travel", 10);
+        newTrans = new CategoryTransaction(preferredCard,"grocery", 10);
         System.out.println(CreditCard.getPointsBal());
         pointsEarned = newTrans.calculatePoints();
+        assert pointsEarned == 50 : "The method calculatePoints returned " + pointsEarned + " but it should have returned 50";
         System.out.println(pointsEarned);
         System.out.println(CreditCard.getPointsBal());
 
