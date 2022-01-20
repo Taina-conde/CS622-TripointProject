06fe31206a688ff02f848b3e8cc490d3c9b8acaf
Taina Conde <tainabatistaconde@gmail.com>
2022-01-20 15:17:04 -0500

    Create empty CreditCardController

---
+++ cards/CreditCard/CreditCardController.java
@@ -0,0 +1,5 @@
+package cards.CreditCard;
+
+public abstract class CreditCardController {
+    
+}
--- cards/CreditCard/CreditCardView.java
+++ cards/CreditCard/CreditCardView.java
@@ -1,5 +1,8 @@
 package cards.CreditCard;
 
-/***/
-public class CreditCardView {
+/**This class handles input and output on the user interface for making a new transaction */
+public abstract class CreditCardView {
+//    public abstract void printTransaction(Transaction newTrans) {
+//
+//    }
 }
