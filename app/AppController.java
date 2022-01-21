dc2da45479353f5dc5c0b3f2af4258cc3a2cca56
Taina Conde <tainabatistaconde@gmail.com>
2022-01-21 18:33:33 -0500

    AppModel - add currentTrans and setCurrentTrans method

--- app/AppController.java
+++ app/AppController.java
@@ -1,5 +1,11 @@
 package app;
 
+import cards.BasicCard;
+import cards.CreditCard;
+import cards.PreferredCard;
+import transactions.CategoryTransaction;
+import transactions.Transaction;
+
 /**This class controls the view and model to support the points account*/
 public class AppController {
     AppModel model = new AppModel();
@@ -8,6 +14,8 @@ public class AppController {
     public AppController() {
         getUsername();
         printInitialMessage();
+        model.addCard(new PreferredCard(model.getCustomer()));
+        model.addCard(new BasicCard(model.getCustomer()));
     }
 
     private void getUsername() {
@@ -17,8 +25,26 @@ public class AppController {
         System.out.println("Hello, " + model.getCustomer()+ "!");
 
     }
+    private void processTransaction() {
+        //for now hard code a new transaction
+        Transaction newTrans = new CategoryTransaction(model.getCardsList().get(1), "travel", 55.60);
+
+
+
+    }
+    /*private void info() {
+        CreditCard card1 = model.getCardsList().get(0);
+        CreditCard card2 = model.getCardsList().get(1);
+        System.out.println(card1.getType());
+        System.out.println(card2.getType());
+
+    }*/
     public static void main(String[] args) {
         AppController controller = new AppController();
+        //TODO : initial menu with options ( view past transactions, make new transaction, view points balance, view points value per card)
+
+
+
 
 
     }
--- app/AppModel.java
+++ app/AppModel.java
@@ -1,15 +1,18 @@
 package app;
 
+import transactions.CategoryTransaction;
 import transactions.Transaction;
 import cards.CreditCard;
 
 import java.util.ArrayList;
 /**This class stores the data needed to use the points account*/
 public class AppModel {
-    private ArrayList<CreditCard> cardsList;
+    private ArrayList<CreditCard> cardsList = new ArrayList<>();
     private String customer;
+    private Transaction currentTrans;
     private int pointsBalance;
     private ArrayList<Transaction> transactionsRecord;
+
 
     //getters
     public ArrayList<CreditCard> getCardsList() {
@@ -17,6 +20,9 @@ public class AppModel {
     }
     public String getCustomer() {
         return customer;
+    }
+    public Transaction getCurrentTrans() {
+        return currentTrans;
     }
     public int getPointsBalance() {return pointsBalance;}
     public ArrayList<Transaction> getTransactionsRecord() {return transactionsRecord;}
@@ -26,6 +32,13 @@ public class AppModel {
         cardsList.add(newCard);
     }
     public void setCustomer(String name) {customer = name;}
+    public void setCurrentTrans(String cardType, String category, double amount ) {
+        for (CreditCard card: cardsList) {
+            if (card.getType().equals(cardType)) {
+                currentTrans = new CategoryTransaction(card, category, amount);
+            }
+        }
+    }
     public void addPoints(int points) {pointsBalance += points;}
     public void removePoints(int points) {pointsBalance -= points;}
     public void addTransaction(Transaction newTrans) {transactionsRecord.add(newTrans);}
