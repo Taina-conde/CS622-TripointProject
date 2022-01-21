3e0eb23bd15320d4f4b29d9c69591da57aa0fb22
Taina Conde <tainabatistaconde@gmail.com>
2022-01-21 15:57:25 -0500

    RecordsReader - printAllRecords method

--- util/RecordsReader.java
+++ util/RecordsReader.java
@@ -1,27 +1,25 @@
 package util;
 
-import transactions.Transaction;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
 /**This class reads the transactionsRecord file*/
 public class RecordsReader {
-    static void recordDisplay(String card, String category, double amount, int points) {
-        System.out.printf("%-15s %-15s %-15d %-15d",
+    protected static void recordDisplay(String card, String category, String amount, String points) {
+        System.out.printf("%-30s %-30s %-30s %-30s\n",
                 card,
                 category,
                 amount,
                 points);
     }
-    ArrayList<Transaction> transRecords;
-    public static void main(String[] args){
+    public static void printAllRecords() {
         try {
             Scanner infile = new Scanner(new File("io/transactionsRecord.txt"));
-            System.out.printf("%-15s %-15s %-15s %-15s","CARD USED", "CATEGORY", "AMOUNT", "POINTS");
-            while (infile.hasNext()) {
-                recordDisplay(infile.next(), infile.next(), infile.nextDouble(), infile.nextInt());
+            System.out.printf("%-30s %-30s %-30s %-30s\n","CARD USED", "CATEGORY", "AMOUNT", "POINTS");
+            while (infile.hasNextLine()) {
+                String[] arr = infile.nextLine().split(", ");
+                recordDisplay(arr[0], arr[1], arr[2], arr[3]);
             }
             infile.close();
         }
--- util_test/RecordsReaderTest.java
+++ util_test/RecordsReaderTest.java
@@ -1,4 +1,10 @@
 package util_test;
 
 public class RecordsReaderTest {
+    public static void recordsDisplayExample(){
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
