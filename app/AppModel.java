ec36fa8d17b77979db44eaeb3a37fb085e52f286
Taina Conde <tainabatistaconde@gmail.com>
2022-01-21 16:52:37 -0500

    AppModel - add attributes

--- app/AppModel.java
+++ app/AppModel.java
@@ -1,4 +1,16 @@
 package app;
 
+import transactions.Transaction;
+import cards.CreditCard;
+
+import java.util.ArrayList;
+
 public class AppModel {
+    private ArrayList<CreditCard> cardsList;
+    private String customer;
+    private int pointsBal;
+    private ArrayList<Transaction> transactionsRecord;
+    
+
+
 }
