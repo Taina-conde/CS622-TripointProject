c05cdd0bc2f4d2e3abe688eed4884d8bd976fe12
Taina Conde <tainabatistaconde@gmail.com>
2022-01-21 17:03:08 -0500

    AppModel : build getters and setters

--- app/AppModel.java
+++ app/AppModel.java
@@ -8,9 +8,29 @@ import java.util.ArrayList;
 public class AppModel {
     private ArrayList<CreditCard> cardsList;
     private String customer;
-    private int pointsBal;
+    private int pointsBalance;
     private ArrayList<Transaction> transactionsRecord;
-    
+
+    //getters
+    public ArrayList<CreditCard> getCardsList() {
+        return cardsList;
+    }
+    public String getCustomer() {
+        return customer;
+    }
+    public int getPointsBalance() {return pointsBalance;}
+    public ArrayList<Transaction> getTransactionsRecord() {return transactionsRecord;}
+
+    //setters
+    public void addCard(CreditCard newCard) {
+        cardsList.add(newCard);
+    }
+    public void setCustomer(String name) {customer = name;}
+    public void addPoints(int points) {pointsBalance += points;}
+    public void removePoints(int points) {pointsBalance -= points;}
+    public void addTransaction(Transaction newTrans) {transactionsRecord.add(newTrans);}
+
+
 
 
 }
