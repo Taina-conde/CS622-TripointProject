c14d7bf16554672126f292b68013b6a721b3b76e
Taina Conde <tainabatistaconde@gmail.com>
2022-01-24 14:33:18 -0500

    create exception incorrectFileNameException

---
+++ src/edu/bu/tbconde/tripoint/exceptions/IncorrectFileNameException.java
@@ -0,0 +1,7 @@
+package edu.bu.tbconde.tripoint.exceptions;
+
+public class IncorrectFileNameException extends Exception{
+    public IncorrectFileNameException(String e, Throwable err) {
+        super(e, err);
+    }
+}
