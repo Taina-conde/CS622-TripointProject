d64aa2ac4a18fd4314d90d9ce19a56b8455a75b0
Taina Conde <tainabatistaconde@gmail.com>
2022-01-25 16:09:14 -0500

    add option to exit in Main Menu

--- src/edu/bu/tbconde/tripoint/util/MainMenu.java
+++ src/edu/bu/tbconde/tripoint/util/MainMenu.java
@@ -13,19 +13,20 @@ public class MainMenu {
         String option1 = "1.Register a new purchase \n ";
         String option2 = "2.View past transactions \n ";
         String option3 = "3.Redeem points \n";
+        String option4 = "4.Exit \n";
         String selectedText;
         int selected;
         System.out.println("What would you like to do?\n " +
               option1 + option2 + option3);
         do {
-            System.out.println("Please, enter 1, 2, or 3.");
+            System.out.println("Please, enter 1, 2, 3, or 4.");
             while(!sc.hasNextInt()) {
                 System.out.println("Please, enter only numbers.");
                 sc.nextLine();
             }
             selected = sc.nextInt();
 
-        } while (!(selected >=1 && selected <=3));
+        } while (!(selected >=1 && selected <=4));
         switch(selected) {
             case 1:
                 selectedText = option1;
@@ -33,9 +34,11 @@ public class MainMenu {
             case 2:
                 selectedText = option2;
                 break;
-            default:
+            case 3:
                 selectedText = option3;
                 break;
+            default:
+                selectedText = "Thank you! Hope to see you again soon!";
         }
         System.out.println("You selected: " + selectedText);
         return selected;
