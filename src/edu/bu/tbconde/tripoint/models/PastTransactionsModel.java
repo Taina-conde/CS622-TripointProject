b888d13c05a30a62f905bc2b467ac802f1069895
Taina Conde <tainabatistaconde@gmail.com>
2022-01-28 17:18:48 -0500

    PastTransactionsView, model and controller

---
+++ src/edu/bu/tbconde/tripoint/models/PastTransactionsModel.java
@@ -0,0 +1,23 @@
+package edu.bu.tbconde.tripoint.models;
+
+public class PastTransactionsModel {
+    private String cardType;
+    private String category;
+    private String amount;
+    private String points;
+
+    //getters
+    public String getCategory() {return category;}
+    public String getAmount() {return amount;}
+    public String getCardType() {return cardType;}
+    public String getPoints(){return points;}
+
+    //setters
+    public void setCategory(String category) {this.category = category;}
+    public void setCardType(String cardType) {this.cardType = cardType;}
+    public void setAmount(String amount) {this.amount= amount;}
+    public void setPoints(String points) { this.points = points;}
+
+
+
+}
---
+++ src/edu/bu/tbconde/tripoint/views/PastTransactionsView.java
@@ -0,0 +1,17 @@
+package edu.bu.tbconde.tripoint.views;
+
+public class PastTransactionsView {
+    public void printRecord(String card, String category, String amount, String points) {
+        System.out.printf("%-30s %-30s %-30s %-30s\n",
+                card,
+                category,
+                amount,
+                points);
+    }
+    public void printHeader() {
+        System.out.println("Your past transactions ...");
+        System.out.println();
+        System.out.printf("%-30s %-30s %-30s %-30s\n","CARD USED", "CATEGORY", "AMOUNT", "POINTS");
+
+    }
+}
