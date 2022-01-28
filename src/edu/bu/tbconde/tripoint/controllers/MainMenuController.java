0f39076ed359613ab61d5d0927721916e58ee244
Taina Conde <tainabatistaconde@gmail.com>
2022-01-28 16:02:07 -0500

    MainMenuController - showMenu method

--- src/edu/bu/tbconde/tripoint/controllers/MainMenuController.java
+++ src/edu/bu/tbconde/tripoint/controllers/MainMenuController.java
@@ -6,4 +6,16 @@ import edu.bu.tbconde.tripoint.views.MainMenuView;
 public class MainMenuController {
     private MainMenuView view = new MainMenuView();
     private MainMenuModel model = new MainMenuModel();
+
+    public void showMenu() {
+        model.setSelectedOption(
+                view.displayOptions(
+                        model.getOption1(),
+                        model.getOption2(),
+                        model.getOption3(),
+                        model.getOption4()
+                )
+        );
+        
+    }
 }
--- src/edu/bu/tbconde/tripoint/models/MainMenuModel.java
+++ src/edu/bu/tbconde/tripoint/models/MainMenuModel.java
@@ -5,6 +5,12 @@ public class MainMenuModel {
     private String option2 = "2.View past transactions \n ";
     private String option3 = "3.Redeem points \n ";
     private String option4 = "4.Exit \n";
-    private String selectedText;
-    private int selected;
+    private int selectedOption;
+    //getters
+    public String getOption1() { return option1;}
+    public String getOption2() { return option2;}
+    public String getOption3() { return option3;}
+    public String getOption4() { return option4;}
+    //setters
+    public void setSelectedOption(int selected) {this.selectedOption = selected;}
 }
