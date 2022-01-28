3e9b7505a87421f93b82ccf516d533cd5dad5164
Taina Conde <tainabatistaconde@gmail.com>
2022-01-28 17:25:23 -0500

    NewTransactionModel getters and setters

--- src/edu/bu/tbconde/tripoint/models/NewTransactionModel.java
+++ src/edu/bu/tbconde/tripoint/models/NewTransactionModel.java
@@ -1,4 +1,20 @@
 package edu.bu.tbconde.tripoint.models;
 
 public class NewTransactionModel {
+    private String cardType;
+    private String category;
+    private double amount;
+
+    //getters
+    public String getCardType() {return cardType;}
+    public String getCategory() {return category;
+    }
+    public double getAmount() {return amount;}
+
+    //setters
+    public void setAmount(double amount) {this.amount = amount; }
+    public void setCategory(String category) {this.category = category;}
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
 }
--- src/edu/bu/tbconde/tripoint/views/NewTransactionView.java
+++ src/edu/bu/tbconde/tripoint/views/NewTransactionView.java
@@ -1,4 +1,44 @@
 package edu.bu.tbconde.tripoint.views;
 
+import java.util.Scanner;
+
 public class NewTransactionView {
+    private Scanner sc = new Scanner(System.in);
+    public String askCardType() {
+        String cardType;
+        System.out.println("Please enter the credit card type used (basic, or preferred)");
+        cardType = sc.nextLine().trim().replaceAll("\\p{P}", "").toLowerCase();
+        while (!(cardType.equals("basic") || cardType.equals("preferred"))){
+            System.out.println(cardType + " is not a valid type.\n" + "Please, enter basic or preferred");
+            cardType = sc.nextLine().trim().replaceAll("\\p{P}", "").toLowerCase();
+        }
+        return cardType;
+    }
+    public String askCategory() {
+        String category;
+        category = sc.nextLine().trim().replaceAll("\\p{P}", "").toLowerCase();
+        while (!(category.equals("dining")
+                || category.equals("travel")
+                || category.equals("online shopping")
+                || category.equals("grocery"))
+        ) {
+            System.out.println(category + " is not a valid category.\n" +
+                    "Please, enter one between dining, travel,  online shopping, or grocery");
+            category = sc.nextLine().trim().replaceAll("\\p{P}", "").toLowerCase();
+        }
+        return category;
+
+    }
+    public double askAmount() {
+        double amount;
+        do {
+            System.out.println("Please enter a positive number greater than 0");
+            while(!sc.hasNextDouble()) {
+                System.out.println("Please enter a number");
+                sc.nextLine();
+            }
+            amount = sc.nextDouble();
+        } while (!(amount > 0));
+        return amount;
+    }
 }
