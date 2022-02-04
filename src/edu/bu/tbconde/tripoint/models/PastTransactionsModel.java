66a975988882fe0f975b72f01a922c66a27ba282
Taina Conde <tainabatistaconde@gmail.com>
2022-02-04 15:37:21 -0500

    remove unused code in PasttransactionsModel

--- src/edu/bu/tbconde/tripoint/models/PastTransactionsModel.java
+++ src/edu/bu/tbconde/tripoint/models/PastTransactionsModel.java
@@ -1,31 +1,16 @@
 package edu.bu.tbconde.tripoint.models;
 
-import edu.bu.tbconde.tripoint.transactions.Transaction;
-import edu.bu.tbconde.tripoint.util.RecordsReader;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
 public class PastTransactionsModel {
-    private RecordsReader reader;
     private String cardType;
     private String category;
     private String amount;
     private String points;
-    private ArrayList<Transaction> records;
-    public PastTransactionsModel() {
-        reader = new RecordsReader();
-        records = new ArrayList<Transaction>();
-    }
 
     //getters
     public String getCategory() {return category;}
     public String getAmount() {return amount;}
     public String getCardType() {return cardType;}
     public String getPoints(){return points;}
-    public ArrayList<Transaction> getRecords() {
-        return records;
-    }
 
     //setters
     public void setCategory(String category) {this.category = category;}
@@ -33,14 +18,4 @@ public class PastTransactionsModel {
     public void setAmount(String amount) {this.amount= amount;}
     public void setPoints(String points) { this.points = points;}
 
-    //read transactions ArrayList from transactionsRecord.dat file
-    public int readAllRecords() {
-        try {
-            this.records = reader.readRecords();
-        }
-        catch (IOException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return records.size();
-    }
 }
