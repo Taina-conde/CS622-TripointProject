3a19acd9849a66cfb339dc5e15c9e916f38221d9
Taina Conde <tainabatistaconde@gmail.com>
2022-02-10 14:28:44 -0500

    Create InitializeRecordsThread

---
+++ src/edu/bu/tbconde/tripoint/util/InitializeRecordsThread.java
@@ -0,0 +1,33 @@
+package edu.bu.tbconde.tripoint.util;
+
+import edu.bu.tbconde.tripoint.transactions.Transaction;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+public class InitializeRecordsThread implements Callable<ArrayList<Transaction>> {
+    private ArrayList<Transaction> records;
+    private RecordsReader reader;
+    private RecordsWriter writer;
+    public InitializeRecordsThread() {
+        records = new ArrayList<Transaction>();
+        reader = new RecordsReader();
+        writer = new RecordsWriter();
+    }
+    public ArrayList<Transaction> call(){
+        try {
+            records = reader.readRecords();
+        }
+        catch (IOException ex) {
+            try { writer.writeRecords(records);}
+            catch(IOException err) {
+                err.printStackTrace();
+            }
+        }
+        catch( ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return records;
+    }
+}
