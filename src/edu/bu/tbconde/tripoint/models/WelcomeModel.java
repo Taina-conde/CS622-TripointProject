9e71362d6221b4955855af7fc95c0c42d6da5840
Taina Conde <tainabatistaconde@gmail.com>
2022-02-17 16:53:04 -0300

    WelcomeModel - add password

--- src/edu/bu/tbconde/tripoint/models/WelcomeModel.java
+++ src/edu/bu/tbconde/tripoint/models/WelcomeModel.java
@@ -4,6 +4,7 @@ public class WelcomeModel {
     private String username;
     private String firstName;
     private String lastName;
+    private String password;
     public WelcomeModel(String username) {
         this.username = username;
     }
@@ -13,7 +14,9 @@ public class WelcomeModel {
     public String getUsername() {return username;}
     public String getFirstName() {return firstName;}
     public String getLastName() {return lastName;}
+    public String getPassword() {return password;}
     public void setUsername(String username) {this.username = username;}
     public void setFirstName(String firstName) {this.firstName = firstName;}
     public void setLastName(String lastName) {this.lastName = lastName;}
+    public void setPassword(String password){this.password = password;}
 }
