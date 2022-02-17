a324d498025dcfbf9d757dd6656c8104670c7ce9
Taina Conde <tainabatistaconde@gmail.com>
2022-02-17 16:46:42 -0300

    UseDataBase - createNewUser

---
+++ src/edu/bu/tbconde/tripoint/database/UseDataBase.java
@@ -0,0 +1,16 @@
+package edu.bu.tbconde.tripoint.database;
+import java.sql.*;
+
+public class UseDataBase {
+    private void createNewUser(Connection conn, String firstName, String lastName, String username, String password) throws SQLException{
+        String sql = "INSERT INTO User(first_name, last_name, username, password) VALUES (?, ?, ?, ?)";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setString(3, username);
+            pstmt.setString(4, password);
+            pstmt.executeUpdate();
+        }
+    }
+
+}
