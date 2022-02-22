6c8e8236f21107df1b5d742d3b6fb00d39383d16
Taina Conde <tainabatistaconde@gmail.com>
2022-02-22 20:33:19 -0300

    UseDataBaseTest

---
+++ tests/edu/bu/tbconde/tripoint/database/UseDataBaseTest.java
@@ -0,0 +1,42 @@
+package edu.bu.tbconde.tripoint.database;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UseDataBaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createNewUser() {
+    }
+
+    @Test
+    void searchUser() {
+    }
+
+    @Test
+    void insertTrans() {
+    }
+
+    @Test
+    void searchUserTransactions() {
+    }
+
+    @Test
+    void searchRecordsByType() {
+    }
+
+    @Test
+    void calculatePointsBalance() {
+    }
+}
