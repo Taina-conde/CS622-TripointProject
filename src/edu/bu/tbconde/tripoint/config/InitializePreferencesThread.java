ede248f55ea5594628d9a268ce29789232d58d0d
Taina Conde <tainabatistaconde@gmail.com>
2022-02-27 15:38:51 -0300

    write initializePreferencesThread

--- src/edu/bu/tbconde/tripoint/config/InitializePreferencesThread.java
+++ src/edu/bu/tbconde/tripoint/config/InitializePreferencesThread.java
@@ -1,4 +1,35 @@
 package edu.bu.tbconde.tripoint.config;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class InitializePreferencesThread {
+    private int userId;
+    private ArrayList<UserPreferences> userPrefsList;
+    private PreferencesReader reader;
+    private PreferencesWriter writer;
+    public InitializePreferencesThread(int userId) {
+        this.userId = userId;
+        userPrefsList = new ArrayList<UserPreferences>();
+        reader = new PreferencesReader();
+        writer = new PreferencesWriter();
+    }
+    public InitializePreferencesThread(int userId, String path) {
+        this.userId = userId;
+        userPrefsList = new ArrayList<UserPreferences>();
+        reader = new PreferencesReader(path);
+        writer = new PreferencesWriter(path);
+    }
+    public ArrayList<UserPreferences> call() throws ClassNotFoundException{
+        try {
+            userPrefsList = reader.readUserPreferences(userId);
+        }
+        catch (IOException ex) {
+            try { writer.writePreferences(userPrefsList);}
+            catch(IOException err) {
+                err.printStackTrace();
+            }
+        }
+        return userPrefsList;
+    }
 }
