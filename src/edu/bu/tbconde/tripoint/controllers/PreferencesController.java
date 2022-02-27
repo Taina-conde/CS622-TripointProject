6c223047c4572e627307643c3e75ad50c7428ddb
Taina Conde <tainabatistaconde@gmail.com>
2022-02-27 12:46:56 -0300

    Add contructor in PreferencesController and initialize PreferencesView

--- src/edu/bu/tbconde/tripoint/controllers/PreferencesController.java
+++ src/edu/bu/tbconde/tripoint/controllers/PreferencesController.java
@@ -1,8 +1,13 @@
 package edu.bu.tbconde.tripoint.controllers;
 
+import edu.bu.tbconde.tripoint.views.PreferencesView;
+
 public class PreferencesController {
-
+    private PreferencesView view;
+    public PreferencesController() {
+        view = new PreferencesView();
+    }
     public void setPreferences() {
-
+        
     }
 }
