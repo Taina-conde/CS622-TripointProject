f8692d1b230c3c5fa0026599addcdc1ecd1c9b2d
Taina Conde <tainabatistaconde@gmail.com>
2022-02-27 12:44:00 -0300

    askOrderBy and askCurrency in PreferencesView

--- src/edu/bu/tbconde/tripoint/views/PreferencesView.java
+++ src/edu/bu/tbconde/tripoint/views/PreferencesView.java
@@ -1,4 +1,58 @@
 package edu.bu.tbconde.tripoint.views;
 
+import java.util.Scanner;
+
 public class PreferencesView {
+    private Scanner sc = new Scanner(System.in);
+
+    public String askCurrency() {
+        int selected;
+        System.out.println("Set currency: \n ");
+        System.out.println("1.Dollar  \n ");
+        System.out.println("2.Euro \n ");
+        do {
+            System.out.println("Please, enter 1, or 2.");
+            while(!sc.hasNextInt()) {
+                System.out.println("Please, enter only numbers.");
+                sc.nextLine();
+            }
+            selected = sc.nextInt();
+
+        } while (!(selected >=1 && selected <=2));
+        if (selected == 1) {
+            return "dollar";
+        }
+        return "euro";
+    }
+    public String askOrderBy() {
+        int selected;
+        System.out.println("Order past transactions by: \n ");
+        System.out.println("1.Most recent  \n ");
+        System.out.println("2.Oldest \n ");
+        System.out.println("3.Greatest amount \n ");
+        System.out.println("4.Lowest amount \n ");
+        System.out.println("5.Type \n ");
+
+        do {
+            System.out.println("Please, enter 1, 2, 3, 4, or 5.");
+            while(!sc.hasNextInt()) {
+                System.out.println("Please, enter only numbers.");
+                sc.nextLine();
+            }
+            selected = sc.nextInt();
+
+        } while (!(selected >=1 && selected <=5));
+        switch(selected) {
+            case 1:
+                return "timestamp DESC";
+            case 2:
+                return "timestamp ASC";
+            case 3:
+                return "amount DESC";
+            case 4:
+                return "amount ASC";
+            default:
+                return "type";
+        }
+    }
 }
